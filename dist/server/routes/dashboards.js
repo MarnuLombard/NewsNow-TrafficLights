fa8a791b9dc156e729ac85cff1269f9167a86fe8
Marnu Lombard <marnulombard@gmail.com>
2022-03-14 07:16:36 +0200

    (Project is git tracked only for backup purposes)

--- dist/server/routes/dashboards.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.routes = void 0;
-const express_1 = require("express");
-const DashboardsController_1 = require("../controllers/DashboardsController");
-exports.routes = (0, express_1.Router)();
-exports.routes.get('/', DashboardsController_1.DashboardsController.index);
-exports.routes.get('/:uuid', DashboardsController_1.DashboardsController.show);
--- dist/server/routes/status.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.routes = void 0;
-const express_1 = require("express");
-const StatusController_1 = require("../controllers/StatusController");
-exports.routes = (0, express_1.Router)();
-exports.routes.get('/', StatusController_1.StatusController.show);
